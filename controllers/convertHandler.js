14840e8dc7f8822f9270096c18670d4352f82ffa
Pavel Jiranek <jiranek35@gmail.com>
2020-07-14 11:16:20 +0200

    feat: getNum from input with unit tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -5,48 +5,73 @@
 *       
 *       
 */
+const R = require('ramda');
+
+const fractionMatcher = /\d+\/\d+/;
+const multipleFractionMatcher = /\d+(\/\d+){2,}/;
+const decimalMatcher = /\d+\.\d+/;
+const numInputMatcher = /^\d+(\/\d+|\.?\d+\/\d+|\.\d+)?/; // matches num with optional decimal and/or fraction
+const DEFAULT_NUM_INPUT = '1'; // 1 is default value when no num is provided
+const INVALID_NUMBER = 'invalid number';
+
+const hasFraction = R.test(fractionMatcher);
+const hasInvalidFraction = R.test(multipleFractionMatcher);
+const getFraction = R.match(fractionMatcher);
+const getFractionValue = R.pipe(getFraction, R.head, eval);
+const isDecimal = R.test(decimalMatcher);
 
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
+  this.getNum = function (input) {
+    if (hasInvalidFraction(input)) {
+      return INVALID_NUMBER;
+    }
+    const [numInput] = input.match(numInputMatcher) || [DEFAULT_NUM_INPUT];
+
+    if (hasFraction(numInput)) {
+      if (isDecimal(numInput)) {
+        const wholeNum = parseInt(numInput, 10);
+        const fractionValue = getFractionValue(numInput);
+        return wholeNum + fractionValue;
+      }
+      return getFractionValue(numInput);
+    }
+    return Number(numInput);
+  }
+
+
+  this.getUnit = function (input) {
+    let result;
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    var result;
-    
+  this.getReturnUnit = function (initUnit) {
+    let result;
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
+    let result;
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    let result;
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,45 +6,50 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chai = require('chai');
-var assert = chai.assert;
-var ConvertHandler = require('../controllers/convertHandler.js');
+const chai = require('chai');
+const assert = chai.assert;
+const ConvertHandler = require('../controllers/convertHandler.js');
 
-var convertHandler = new ConvertHandler();
+const convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-  
-  suite('Function convertHandler.getNum(input)', function() {
-    
-    test('Whole number input', function(done) {
-      var input = '32L';
-      assert.equal(convertHandler.getNum(input),32);
+suite('Unit Tests', function () {
+
+  suite('Function convertHandler.getNum(input)', function () {
+
+    test('Whole number input', function (done) {
+      const input = '32L';
+      assert.strictEqual(convertHandler.getNum(input), 32);
+      done();
+    });
+
+    test('Decimal Input', function(done) {
+      const input = '4.5gal';
+      assert.strictEqual(convertHandler.getNum(input), 4.5);
       done();
     });
     
-    test('Decimal Input', function(done) {
-      
-      //done();
-    });
-    
     test('Fractional Input', function(done) {
-      
-      //done();
+      const input = '1/2km';
+      assert.strictEqual(convertHandler.getNum(input), 0.5);
+      done();
     });
     
     test('Fractional Input w/ Decimal', function(done) {
-      
-      //done();
+      const input = '5.5/4lbs';
+      assert.strictEqual(convertHandler.getNum(input), 6.25);
+      done();
     });
     
     test('Invalid Input (double fraction)', function(done) {
-      
-      //done();
+      const input = '5/4/2lbs';
+      assert.strictEqual(convertHandler.getNum(input), 'invalid number');
+      done();
     });
     
     test('No Numerical Input', function(done) {
-      
-      //done();
+      const input = 'km';
+      assert.strictEqual(convertHandler.getNum(input), 1);
+      done();
     }); 
     
   });
