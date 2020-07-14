5b884866f15ad5a05d19017e54785404b6ea9263
Pavel Jiranek <jiranek35@gmail.com>
2020-07-14 15:10:24 +0200

    refactor: remove redundant function declarations, and use methods in convertHandler

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,19 +1,19 @@
 "use strict";
 /*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
-const R = require('ramda');
-const RA = require('ramda-adjunct');
+ *
+ *
+ *       Complete the handler logic below
+ *
+ *
+ */
+const R = require("ramda");
+const RA = require("ramda-adjunct");
 
 const fractionMatcher = /\d+\/\d+/;
 const denominatorMatcher = /\/\d+/g;
 const decimalMatcher = /\d+\.\d+/;
 const numInputMatcher = /^\d+(\/\d+|\.?\d+\/\d+|\.\d+)?/; // matches num with optional decimal and/or fraction
-const DEFAULT_NUM_INPUT = '1'; // 1 is default value when no num is provided
+const DEFAULT_NUM_INPUT = "1"; // 1 is default value when no num is provided
 
 const hasFraction = R.test(fractionMatcher);
 const hasInvalidFraction = R.pipe(
@@ -22,34 +22,52 @@ const hasInvalidFraction = R.pipe(
 );
 const roundToFixed5 = num => Number(num.toFixed(5));
 const getFraction = R.match(fractionMatcher);
-const getFractionValue = R.pipe(getFraction, R.head, eval, roundToFixed5);
+const getFractionValue = R.pipe(
+    getFraction,
+    R.head,
+    eval,
+    roundToFixed5,
+);
 const isDecimal = R.test(decimalMatcher);
 
-const VALID_UNITS = ['gal', 'l', 'mi', 'km', 'lbs', 'kg', 'GAL', 'L', 'MI', 'KM', 'LBS', 'KG'];
+const VALID_UNITS = [
+  "gal",
+  "l",
+  "mi",
+  "km",
+  "lbs",
+  "kg",
+  "GAL",
+  "L",
+  "MI",
+  "KM",
+  "LBS",
+  "KG",
+];
 const UNITS_CONVERSION_MAP = {
-  'gal': 'l',
-  'l': 'gal',
-  'mi': 'km',
-  'km': 'mi',
-  'lbs': 'kg',
-  'kg': 'lbs',
-}
+  gal: "l",
+  l: "gal",
+  mi: "km",
+  km: "mi",
+  lbs: "kg",
+  kg: "lbs",
+};
 
 const UNITS_FULLNAME_MAP = {
-  'gal': 'gallons',
-  'l': 'liters',
-  'mi': 'miles',
-  'km': 'kilometers',
-  'lbs': 'pounds',
-  'kg': 'kilograms',
-}
+  gal: "gallons",
+  l: "liters",
+  mi: "miles",
+  km: "kilometers",
+  lbs: "pounds",
+  kg: "kilograms",
+};
 const UNITS = {
-  GAL: 'gal',
-  L: 'l',
-  MI: 'mi',
-  KM: 'km',
-  LBS: 'lbs',
-  KG: 'kg',
+  GAL: "gal",
+  L: "l",
+  MI: "mi",
+  KM: "km",
+  LBS: "lbs",
+  KG: "kg",
 };
 const unitMatcher = /[a-zA-Z]+/;
 
@@ -59,11 +77,11 @@ const MI_TO_KM = 1.60934;
 
 class ConvertHandler {
   constructor() {
-    this.INVALID_NUMBER = 'invalid number';
-    this.INVALID_UNIT = 'invalid unit';
+    this.INVALID_NUMBER = "invalid number";
+    this.INVALID_UNIT = "invalid unit";
   }
 
-  getNum = function (input) {
+  getNum(input) {
     if (hasInvalidFraction(input)) {
       return this.INVALID_NUMBER;
     }
@@ -80,26 +98,25 @@ class ConvertHandler {
     return Number(numInput);
   }
 
-
-  getUnit = function (input) {
-    const [unit] = input.match(unitMatcher) || [''];
+  getUnit(input) {
+    const [unit] = input.match(unitMatcher) || [""];
 
     return VALID_UNITS.includes(unit) ? unit : this.INVALID_UNIT;
-  };
+  }
 
-  getReturnUnit = function (initUnit) {
+  getReturnUnit(initUnit) {
     const normalizedUnit = initUnit.toLowerCase();
 
     return UNITS_CONVERSION_MAP[normalizedUnit];
-  };
+  }
 
-  spellOutUnit = function (unit) {
+  spellOutUnit(unit) {
     const normalizedUnit = unit.toLowerCase();
 
     return UNITS_FULLNAME_MAP[normalizedUnit];
-  };
+  }
 
-  convert = function (initNum, initUnit) {
+  convert(initNum, initUnit) {
     const normalizedUnit = initUnit.toLowerCase();
     let result;
 
@@ -130,12 +147,11 @@ class ConvertHandler {
       }
     }
     return roundToFixed5(result);
-  };
+  }
 
-  getString = function (initNum, initUnit, returnNum, returnUnit) {
+  getString(initNum, initUnit, returnNum, returnUnit) {
     return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
-  };
-
+  }
 }
 
 module.exports = ConvertHandler;
